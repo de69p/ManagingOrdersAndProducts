d053db4ac36b7da00fe7d9801527b75a03ce68c2
arsendeputat <depaars@gmail.com>
2023-04-04 09:00:43 +0300

    Initial commit

---
+++ src/main/java/com/example/managingordersandproducts/ManagingOrdersAndProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.managingordersandproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingOrdersAndProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingOrdersAndProductsApplication.class, args);
+    }
+
+}
